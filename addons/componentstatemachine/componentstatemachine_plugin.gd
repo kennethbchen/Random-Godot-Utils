2220c6c42b33f9b0515a179d3ea41b09ab0f5a92
kennethbchen <kennethbchen@gmail.com>
2023-08-08 20:14:49 -0400

    fixed component state machine plugin error

--- addons/componentstatemachine/componentstatemachine_plugin.gd
+++ addons/componentstatemachine/componentstatemachine_plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("ComponentStateMachine", "Node", preload("res://addons/componentstatemachine/component_state_machine/ComponentStateMachine.gd"), preload("component_state_machine/ComponentStateMachine.svg"))
+	add_custom_type("ComponentStateMachine", "Node", preload("res://addons/componentstatemachine/component_state_machine/component_state_machine.gd"), preload("component_state_machine/ComponentStateMachine.svg"))
 
 func _exit_tree():
 	remove_custom_type("ComponentStateMachine")
